d98e152ff0e773d4974ae80c10d5138d8e19930e
feshalnaguji <fesalnaguji@gmail.com>
2023-04-11 22:07:12 +0530

    simple get and post request using express

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side');
+  res.status(200).json({
+    message: 'Hello from the server side',
+    app: 'natours',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint');
 });
 
 // start a server

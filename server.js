b1f209031b3e5bd5f6183ba3b30bea4ec09bdb48
feshalnaguji <fesalnaguji@gmail.com>
2023-04-12 21:24:37 +0530

    created first schema, model and tour using mongoose

--- server.js
+++ server.js
@@ -17,13 +17,52 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection established');
   });
 
+// Creating mongoose schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // validator
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Create model from schema
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Create tour from model
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 497,
+});
+
+// save tour to database which will return a promise
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR :', err);
+  });
+
 // start a server
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
+  // callback function is called as soon as server starts listening
   console.log(`App running or port ${port}...`);
-}); // callback function is called as soon as server starts listening
+});

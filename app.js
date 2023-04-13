2454de748435f8bece462c282b12e3a9c3b74573
feshalnaguji <fesalnaguji@gmail.com>
2023-04-13 20:35:51 +0530

    Virtual properties added

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 // Routes
 
-app.use('/api/v1/tours', tourRouter); // mounting a router
+app.use('/api/v1/tours', tourRouter); // creating sub routers
 app.use('/api/v1/users', userRouter);
 
 // start a server

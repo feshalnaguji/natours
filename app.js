8d10d7e2b14f9e6ec2b0c2fc3adbfff998503a36
feshalnaguji <fesalnaguji@gmail.com>
2023-04-14 20:44:13 +0530

    Data Sanitization Against NoSQL query injection and XSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +34,12 @@ app.use('/api', limiter);
 
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // bodyParser
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize()); // filter out all of the $ signs and dots from req body, query and params
+
+// Data sanitization against XSS
+app.use(xss()); // converts html symbols to normal entity
 
 // accessing static files like html css images etc.
 app.use(express.static(`${__dirname}/public`));

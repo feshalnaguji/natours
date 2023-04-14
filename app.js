534fb584aa270b34170951c88995807d6ea6572c
feshalnaguji <fesalnaguji@gmail.com>
2023-04-14 22:50:14 +0530

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes.js');
 const userRouter = require('./routes/userRoutes.js');
+const reviewRouter = require('./routes/reviewRoutes.js');
 
 const app = express();
 
@@ -75,6 +76,7 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter); // creating sub routers
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Handling all the unhandled routes
 app.all('*', (req, res, next) => {

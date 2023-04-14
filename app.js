cdfed1a5e0864b3568ccf719e00874a360906e10
feshalnaguji <fesalnaguji@gmail.com>
2023-04-14 20:35:07 +0530

    Setting Security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,9 +10,13 @@ const userRouter = require('./routes/userRoutes.js');
 
 const app = express();
 
-console.log(process.env.NODE_ENV);
-// GLOBAL MIDDLEWARES
-// middlewares
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP header
+app.use(helmet());
+
+// Development logging
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // for getting request data in log
 }
@@ -25,8 +30,11 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json()); // bodyParser
-app.use(express.static(`${__dirname}/public`)); // accessing static files like html css images etc.
+// Body Parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); // bodyParser
+
+// accessing static files like html css images etc.
+app.use(express.static(`${__dirname}/public`));
 
 // custom middlewares
 // app.use((req, res, next) => {
@@ -34,6 +42,7 @@ app.use(express.static(`${__dirname}/public`)); // accessing static files like h
 //   next();
 // });
 
+// custom test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   console.log(req.headers);

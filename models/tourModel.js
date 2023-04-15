700214b555cd92783d664e219aeefc2aefb103f9
feshalnaguji <fesalnaguji@gmail.com>
2023-04-15 15:21:11 +0530

    Prevent Duplicate Reviews using Compound Index

--- models/tourModel.js
+++ models/tourModel.js
@@ -37,6 +37,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // setter function run each time when there is a new value of ratingsAverage field
     },
     ratingsQuantity: {
       type: Number,

483293567430809ea645f09e33c3790964a3f689
feshalnaguji <fesalnaguji@gmail.com>
2023-04-15 15:11:59 +0530

    Calculated Average Rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -48,6 +49,58 @@ reviewSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+// static method, works on schema but not on documents like instance method
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // this key points to current Model as it's a static method
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log(stats);
+
+  // check af there are stats in array or not
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  // this keyword points to current model/review
+  this.constructor.calcAverageRatings(this.tour); // points to model who created the document
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.currentRatingDoc = await this.findOne(); // get the current document by executing query
+  // console.log(this.currentRatingDoc);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.currentRatingDoc.constructor.calcAverageRatings(
+    this.currentRatingDoc.tour
+  );
 });
 
 const Review = mongoose.model('Review', reviewSchema);
